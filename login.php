8cb23ea51ea00889f39753d0ac829f93f746dd6e
sdragonpt <sergionunoribeiro@gmail.com>
2024-09-10 16:22:49 +0100

    Update login.php

--- login.php
+++ login.php
@@ -61,7 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             color: #004080;
         }
         .login-container input {
-            width: 94%;
+            width: 93%;
             padding: 10px;
             margin: 10px 0;
             border: 1px solid #ddd;
