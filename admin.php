c376885a682f44ee983d93abee515ccf3b83c3b3
sdragonpt <sergionunoribeiro@gmail.com>
2024-09-10 19:29:57 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -263,7 +263,7 @@ if (isset($_GET['logout'])) {
                     <?php while ($categoria = $categorias_result->fetch_assoc()): ?>
                         <label style="border: 1px solid; border-radius: 3px; padding: 4px; font-size: 14px;">
                             <input style="width: 13px; margin: 0px;" type="checkbox" name="categorias[]" value="<?php echo $categoria['id']; ?>">
-                            <div style="margin-bottom: 2px;"><?php echo htmlspecialchars($categoria['nome']); ?></div>
+                            <div style="margin-bottom: 2px; float: left;"><?php echo htmlspecialchars($categoria['nome']); ?></div>
                         </label>
                     <?php endwhile; ?>
                 </div>
